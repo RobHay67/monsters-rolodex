ff8569de8394381715a91c9d4982a75fa28302ff
Rob Hay <rob@blueswoop.com>
2020-05-20 16:13:36 +1000

    Updates

---
+++ precache-manifest.b095b320349e8d6284a5fb69bce628c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425d4368da0573ab4bbbfa68f7b50cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a956ea03717698da50a",
+    "url": "/static/css/main.612fa057.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a956ea03717698da50a",
+    "url": "/static/js/main.1483df75.chunk.js"
+  },
+  {
+    "revision": "0b26c044026ecf183e79",
+    "url": "/static/js/runtime-main.464fd773.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
