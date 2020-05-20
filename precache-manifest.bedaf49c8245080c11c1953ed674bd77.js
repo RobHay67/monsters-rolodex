6e63178c0d5415ba991af206146e6336d2707baf
Rob Hay <rob@blueswoop.com>
2020-05-20 16:13:58 +1000

    Updates

---
+++ precache-manifest.bedaf49c8245080c11c1953ed674bd77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5208760dd47bfbe74cca811bc564412",
+    "url": "/RobHay67/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b4bc41422a0103cb66d",
+    "url": "/RobHay67/monsters-rolodex/static/css/main.612fa057.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/RobHay67/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobHay67/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4bc41422a0103cb66d",
+    "url": "/RobHay67/monsters-rolodex/static/js/main.035d783f.chunk.js"
+  },
+  {
+    "revision": "9cc62a02413eb1d3bea3",
+    "url": "/RobHay67/monsters-rolodex/static/js/runtime-main.37e46ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RobHay67/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b095b320349e8d6284a5fb69bce628c1.js"
+  "/RobHay67/monsters-rolodex/precache-manifest.bedaf49c8245080c11c1953ed674bd77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RobHay67/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
